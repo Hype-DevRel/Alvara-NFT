60b8e3b4a620dd7f96139e8a8641583669163083
haimantika <hmitra@digitalocean.com>
2025-08-01 00:13:12 +0530

    Added images and FAQ section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,17 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit";
-import { Header, Footer, AvatarMinter } from "@/components";
+import { Header, Footer, AvatarMinter, FloatingFAQ } from "@/components";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-[#13061F] lg:h-screen lg:overflow-hidden">
-      <div className="relative z-10 min-h-screen lg:h-full flex flex-col">
-        <Header />
-        <div className="flex-1 flex items-center justify-center pt-20 pb-4 px-4 sm:px-6 lg:py-0 lg:px-0">
+    <main className="min-h-screen bg-[#13061F]">
+      <Header />
+      <div className="pt-20 md:pt-24">
+        <div className="flex items-center justify-center py-8 px-4 sm:px-6 lg:py-12 lg:px-0">
           <AvatarMinter />
         </div>
         <Footer />
       </div>
+      <FloatingFAQ />
     </main>
   );
 }
